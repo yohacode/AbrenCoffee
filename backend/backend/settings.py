4974dbf79e61c7e43f8fb2df45d50e33187ffeab
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-15 09:15:14 -0700

    fullstack test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,10 +14,10 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
