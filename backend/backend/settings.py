dddb5eeb4b5b6b6affb33eab4d6893bb3456e8ec
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-16 09:01:10 -0700

    fullstack test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
