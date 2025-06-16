e4eda99dccf7c84ddebf10d710a64fec8b1ac76f
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-16 07:31:13 -0700

    fullstack test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,6 @@ DEBUG = os.getenv('DEBUG')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
