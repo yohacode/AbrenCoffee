8b5b2cc727aea5d7ec410867f9289ae0f2ecf433
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-26 14:40:41 -0700

    render r

--- frontend/src/pages/blog/blogDetail.tsx
+++ frontend/src/pages/blog/blogDetail.tsx
@@ -17,7 +17,7 @@ interface BlogDetail {
   categories: string[];
 }
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 interface Comment {
   id: number;
