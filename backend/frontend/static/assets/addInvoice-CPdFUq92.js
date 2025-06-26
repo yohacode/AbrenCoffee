d47463703ba4246e26eda6fd9d6b312b13d3a729
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-26 10:54:27 -0700

    done r

---
+++ backend/frontend/static/assets/addInvoice-CPdFUq92.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-S1SBBQp1.js";const r=()=>t.jsx("div",{});export{r as default};
---
+++ backend/frontend/static/assets/orderCreate-pQ0zGgcQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-S1SBBQp1.js";const e=()=>r.jsx("div",{});export{e as default};
