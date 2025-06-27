6ecbeba9a400cacee0ccbf73a3846144ad9be8af
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-27 12:26:15 -0700

    rendr n

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -193,9 +193,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
