a6c0b13d0a450c21c1268f1740df5019b8b0917d
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-28 15:18:36 -0700

    restart

--- backend/static/assets/orderCreate-oFYmfEnV.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CQVHAiK1.js";const e=()=>r.jsx("div",{});export{e as default};
---
+++ backend/staticfiles/assets/orderCreate-B1_DSpIQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C5IzVONI.js";const e=()=>r.jsx("div",{});export{e as default};
