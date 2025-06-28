693fd4d70a5994da186044b8295dcd6674e5da26
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-28 15:28:18 -0700

    restart

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -3,7 +3,5 @@ from django.views.generic import TemplateView
 from django.conf import settings
 
 def index(request):
-    return render(request, 'index.html',{
-        "API_BASE_URL": settings.API_BASE_URL
-    })
+    return render(request, 'index.html')
 
