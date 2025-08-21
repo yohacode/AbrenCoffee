cd4633b67d3ea2e39248a4b01ebfa37fcd182e40
Yohacodes <johnpro3269@gmail.com>
2025-08-21 16:48:28 +0300

    Update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,7 +91,7 @@ REST_FRAMEWORK = {
 SESSION_ENGINE = os.getenv('SESSION_ENGINE')
 CORS_ALLOW_CREDENTIALS = True
 
-SESSION_COOKIE_SAMESITE = 'None'     # Allow cross-site requests
+SESSION_COOKIE_SAMESITE = None     # Allow cross-site requests
 SESSION_COOKIE_SECURE = False         # Required for 'None' to work in modern browsers
 
 CSRF_COOKIE_SAMESITE = 'None'
